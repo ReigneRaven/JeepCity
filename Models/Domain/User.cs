c15d27717ba0b414b7678b8e1ef8bfd38d1ca4dd
Raven Ramos <139956702+ReigneRaven@users.noreply.github.com>
2023-07-24 11:31:52 +0800

    Add files via upload

---
+++ Models/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace JeepCity.Models.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string feedback { get; set; }
+    }
+}
